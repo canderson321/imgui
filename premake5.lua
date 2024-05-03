fbf670ffaad4dc32373e33fae1014d96bf8f1e91
Cameron Anderson <chamburg321@gmail.com>
2024-05-02 18:12:46 -0700

    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,35 @@
+-- IMGUI --
+
+project "IMGUI"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    systemversion "latest"
+    staticruntime "On"
+    
+    outdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    targetdir ("build/" .. outdir .. "/%{prj.name}")
+    objdir    ("obj/" .. outdir .. "/%{prj.name}")
+    
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
